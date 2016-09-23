119df5e52fd528e79e70d47bd789ac8d3fd916e4
Christoffer Winterkvist <christoffer@hyper.no>
2016-09-23 09:46:56 +0200

    Add tvOS target

---
+++ Cache-tvOS/Cache-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Cache-tvOS.h
+//  Cache-tvOS
+//
+//  Created by Christoffer Winterkvist on 9/23/16.
+//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cache-tvOS.
+FOUNDATION_EXPORT double Cache_tvOSVersionNumber;
+
+//! Project version string for Cache-tvOS.
+FOUNDATION_EXPORT const unsigned char Cache_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cache_tvOS/PublicHeader.h>
+
+
